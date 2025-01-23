898ea3d73d0b76bf6381225b307b248087b81eb6
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2025-01-23 23:35:10 +0300

    Fixed multiline string

--- app_python/src/main.py
+++ app_python/src/main.py
@@ -17,15 +17,14 @@ async def index():
     curr_time = datetime.now(pytz.timezone(settings.TIMEZONE))
     formatted_time = curr_time.strftime(settings.DATETIME_FORMAT)
 
-    html_content = f"""
-    <html>
-        <head>
-            <title>{settings.TIMEZONE} Time</title>
-        </head>
-        <body>
-            <h1>Current time in {settings.TIMEZONE}: {formatted_time}</h1>
-        </body>
-    </html>
+    html_content = f"""<html>
+    <head>
+        <title>{settings.TIMEZONE} Time</title>
+    </head>
+    <body>
+        <h1>Current time in {settings.TIMEZONE}: {formatted_time}</h1>
+    </body>
+</html>
     """
 
     return HTMLResponse(content=html_content, status_code=status.HTTP_200_OK)
