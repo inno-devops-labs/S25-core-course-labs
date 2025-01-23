197a6b9753531f43199c18ca7f69f5eca8aaabf8
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2025-01-23 16:18:52 +0300

    feat: add html template

--- app_python/main.py
+++ app_python/main.py
@@ -5,11 +5,14 @@ This module defines a simple FastAPI application with two endpoints:
 Author: Sviatoslav Sviatkin (s.sviatkin@innopolis.university)
 """
 
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
@@ -23,16 +26,17 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, item_id: str):
     """
     Handle the endpoint for retrieving item details.
 
     Args:
         item_id (int): The ID of the item to retrieve.
-        q (Union[str, None], optional): An optional query parameter.
 
     Returns:
-        dict: A dictionary containing the item ID and the optional query parameter.
+        HTMLResponse: A HTML page containing the item ID.
     """
-    return {"item_id": item_id, "q": q}
+    return templates.TemplateResponse(
+        request=request, name="item.html", context={"id": item_id}
+    )
