587e4eeebaae28bcfec87fad0dd9597cf2fa81ca
l.smirnova <l.smirnova@innopolis.university>
2025-01-24 21:30:44 +0300

    added styles, fixed python code, wrote md

---
+++ app-python/PYTHON.md
@@ -0,0 +1,7 @@
+# Framework:
+
+Flask was chosen as a framework for this project as it is simple yet functional and light-weighted.
+
+# Best practices:
+
+1. Code matches PEP-8 standart, so it is easily readable.
--- app-python/app.py
+++ app-python/app.py
@@ -4,11 +4,13 @@ import pytz
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     moscow_tz = pytz.timezone("Europe/Moscow")
     current_time = datetime.now(moscow_tz).strftime("%H:%M:%S")
     return render_template("index.html", time=current_time)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
