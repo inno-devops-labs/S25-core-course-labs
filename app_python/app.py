b54dc50e8511efc11c7a8b834412319c59271881
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2025-01-24 11:11:49 +0300

    chore: run black formatter on project

--- app_python/app.py
+++ app_python/app.py
@@ -3,10 +3,12 @@ from utils.time_util import get_current_time_in_moscow
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     current_time = get_current_time_in_moscow()
     return render_template("index.html", current_time=current_time)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app_python/tests/test_app.py
+++ app_python/tests/test_app.py
@@ -7,6 +7,7 @@ def client():
     with app.test_client() as client:
         yield client
 
+
 def test_home_page(client):
     response = client.get("/")
     assert response.status_code == 200
