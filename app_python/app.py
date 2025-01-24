1df01597d744e9b36cf5682172626b9e49e07604
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2025-01-24 10:56:51 +0300

    feat: implement web app

---
+++ app_python/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from utils.time_util import get_current_time_in_moscow
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    current_time = get_current_time_in_moscow()
+    return render_template("index.html", current_time=current_time)
+
+if __name__ == "__main__":
+    app.run(debug=True)
