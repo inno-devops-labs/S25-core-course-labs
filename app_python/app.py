643cfa58ad5c817aab96170d3c75926fb723b4b9
Milanaaaa <milanasirozhova@yandex.ru>
2025-01-25 22:31:07 +0300

    Formatted code

--- app_python/app.py
+++ app_python/app.py
@@ -1,8 +1,10 @@
-from flask import Flask
 from datetime import datetime
+
 import pytz
+from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def current_time():
@@ -10,5 +12,6 @@ def current_time():
     current_time = datetime.now(moscow_tz).strftime("%Y-%m-%d %H:%M:%S")
     return f"<h1>Current time in Moscow: {current_time}</h1>"
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
