381615b0e50c1c77b4bcd5ded63b366dbe0fffe4
Azamat Bayramov <bayramov.azamat04@gmail.com>
2025-01-25 16:56:21 +0300

    Improve Go application

--- app_go/main.go
+++ app_go/main.go
@@ -1,42 +1,63 @@
 package main
 
 import (
-	"time"
-	"net/http"
-	"io/ioutil"
 	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
+const (
+	// Port to run the application
+	appPort = ":8002"
+
+	// Date format to display
+	dateFormat = "2006-01-02"
+
+	// Timezone to display the date
+	timezone = "Europe/Moscow"
+
+	// HTML file with the web page
+	HTMLFileName = "index.html"
+)
+
+// Response model with a date field
 type DateResponse struct {
 	Date string `json:"date"`
 }
 
+// Returns the HTML web page that displays the current date
+// in the specified timezone using API
+func getCurrentDateHTML(w http.ResponseWriter, r *http.Request) {
+	htmlContent, err := ioutil.ReadFile(HTMLFileName)
+
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	w.Write(htmlContent)
+}
+
+// Returns the current date in the specified timezone
+func getCurrentDate(w http.ResponseWriter, r *http.Request) {
+	location, err := time.LoadLocation(timezone)
+
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	response := DateResponse{Date: time.Now().In(location).Format(dateFormat)}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		htmlContent, err := ioutil.ReadFile("index.html")
+	http.HandleFunc("/", getCurrentDateHTML)
+	http.HandleFunc("/api/date", getCurrentDate)
 
-		if err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "text/html")
-		w.Write(htmlContent)
-	})
-
-	http.HandleFunc("/api/date", func(w http.ResponseWriter, r *http.Request) {
-		location, err := time.LoadLocation("Europe/Moscow")
-
-		if err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
-
-		response := DateResponse{Date: time.Now().In(location).Format("2006-01-02"),}
-
-		w.Header().Set("Content-Type", "application/json")
-	    json.NewEncoder(w).Encode(response)
-	})
-
-	http.ListenAndServe(":8002", nil)
+	http.ListenAndServe(appPort, nil)
 }
