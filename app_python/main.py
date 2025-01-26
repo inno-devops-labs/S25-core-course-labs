21a169d5fdd5052ba431d82970c14f5a1ab6dea8
Kamil116 <kamil.mirgasimoff@yandex.ru>
2025-01-26 17:25:03 +0300

    feat: main functionality && readme

---
+++ app_python/main.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+import pytz
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    # Getting Moscow time
+    msk_time = get_moscow_time()
+    return render_template('hello.html', time=msk_time)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html')
+
+
+def get_moscow_time():
+    return datetime.now(pytz.timezone('Europe/Moscow')).strftime('%d-%m-%Y %H:%M:%S')
