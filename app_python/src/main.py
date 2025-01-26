37a432d63c040ea4635745d243ea9d396331b4bc
KonstantinPetrovichQWERTY <skatkov24@mail.ru>
2025-01-26 22:22:46 +0300

    feat: add logging middleware

--- app_python/src/main.py
+++ app_python/src/main.py
@@ -1,15 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from datetime import datetime
 import pytz
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    logger.info(
+        f"Incoming request: {request.method} {request.url.path} from {request.client.host}"
+    )
+
+    response = await call_next(request)
+
+    logger.info(f"Outgoing response: Status {response.status_code}")
+    return response
 
 
 @app.get("/get_moscow_time")
 def get_moscow_time():
     """
     Get the current time in Moscow, Russia.
-    
+
     Returns:
         dict:
             - 'moscow_time': string in 'YYYY-MM-DD HH:MM:SS' format;
