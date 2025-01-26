1e0c158fae019c15569181b76a38098809a1591d
Nickolaus-899 <kolya.petukhov.03@mail.ru>
2025-01-26 15:43:58 +0300

    first task is done

--- app_python/app.py
+++ app_python/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
 import pytz
@@ -6,20 +6,31 @@ import pytz
 
 app = Flask(__name__)
 
-def get_page_text(cur_time, city="Moscow"):
-    return f"""
-        <h3>This is an application for the DevOps core course</h3>
-        <h1>The current time in {city} is:</h1>
-        <h1>{cur_time}</h1>
-    """
+# global vars that represent a city to display time in
+CITY = "Moscow"
+TIMEZONE = "Europe/Moscow"
 
 
+# function for returning date and time in the given timezone
+def get_current_time(str_timezone):
+    try:
+        tz = pytz.timezone(str_timezone)
+        return datetime.now(tz).strftime('%d/%m/%Y %H:%M:%S')
+    except pytz.UnknownTimeZoneError:
+        app.logger.error(f"Unknown time zone: {str_timezone}")
+        return None
+
+
+# rendering the home (main) page
 @app.route('/')
 def home():
-    time_zone = pytz.timezone("Europe/Moscow")
+    time_zone = get_current_time(TIMEZONE)
+    if time_zone is None:
+        return "Invalid given timezone", 400
+    
     cur_time = datetime.now(time_zone).strftime('%d/%m/%Y %H:%M:%S')
 
-    return get_page_text(cur_time=cur_time)
+    return render_template('home.html', cur_time=cur_time, city=CITY)
 
 
 if __name__ == '__main__':
