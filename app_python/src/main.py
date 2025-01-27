9fcd71272995d9cb7952d07d49d66a9ec59585d2
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2025-01-27 10:51:38 +0300

    Added style to app_python

--- app_python/src/main.py
+++ app_python/src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from .config import settings
 from .routes import router
@@ -7,4 +8,7 @@ app = FastAPI(
     title=settings.APP_TITLE,
     version=settings.APP_VERSION,
 )
+
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
+
 app.include_router(router)
