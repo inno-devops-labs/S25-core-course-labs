41e4da15b52cee392f8471cc3ae2c6c5e030c304
Nika <nicachehonina@gmail.com>
2025-01-27 21:13:24 +0300

    fix quality"

--- app_go/main.go
+++ app_go/main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
-  "html/template"
-  "log"
-  "net/http"
-  "time"
-  "fmt"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
 )
 
 func main() {
-  r := gin.Default()
+	r := gin.Default()
 
-  r.GET("/", func(c *gin.Context) {
-    log.Println("Request received at root endpoint")
+	r.GET("/", func(c *gin.Context) {
+		log.Println("Request received at root endpoint")
 
-    // Get the current date in Moscow timezone
-    loc, err := time.LoadLocation("Europe/Moscow")
-    if err != nil {
-      log.Printf("Error loading timezone: %v", err)
-      c.String(http.StatusInternalServerError, "Internal Server Error")
-      return
-    }
-    currentDate := time.Now().In(loc)
-	y, m, d := currentDate.Date()
-	formattedDate := fmt.Sprintf("%d %s %d", y, m, d)
+		// Get the current date in Moscow timezone
+		loc, err := time.LoadLocation("Europe/Moscow")
+		if err != nil {
+			log.Printf("Error loading timezone: %v", err)
+			c.String(http.StatusInternalServerError, "Internal Server Error")
+			return
+		}
+		currentDate := time.Now().In(loc)
+		y, m, d := currentDate.Date()
+		formattedDate := fmt.Sprintf("%d %s %d", y, m, d)
 
-    // Generate the HTML response
-    tmpl := `
+		// Generate the HTML response
+		tmpl := `
     <html>
       <head>
         <title>Moscow Date</title>
@@ -38,20 +38,20 @@ func main() {
       </body>
     </html>`
 
-    parsedTmpl, err := template.New("webpage").Parse(tmpl)
-    if err != nil {
-      log.Printf("Error parsing template: %v", err)
-      c.String(http.StatusInternalServerError, "Internal Server Error")
-      return
-    }
+		parsedTmpl, err := template.New("webpage").Parse(tmpl)
+		if err != nil {
+			log.Printf("Error parsing template: %v", err)
+			c.String(http.StatusInternalServerError, "Internal Server Error")
+			return
+		}
 
-    c.Header("Content-Type", "text/html; charset=utf-8")
-    if err := parsedTmpl.Execute(c.Writer, formattedDate); err != nil {
-      log.Printf("Error executing template: %v", err)
-      c.String(http.StatusInternalServerError, "Internal Server Error")
-      return
-    }
-  })
+		c.Header("Content-Type", "text/html; charset=utf-8")
+		if err := parsedTmpl.Execute(c.Writer, formattedDate); err != nil {
+			log.Printf("Error executing template: %v", err)
+			c.String(http.StatusInternalServerError, "Internal Server Error")
+			return
+		}
+	})
 
-  r.Run(":8080")
+	r.Run(":8080")
 }
