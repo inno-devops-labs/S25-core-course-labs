4f1a2ad04f82661a2b0596bb4d223adb90c35387
Nika <nicachehonina@gmail.com>
2025-01-27 16:22:14 +0300

    add logging

--- app_python/main.py
+++ app_python/main.py
@@ -2,10 +2,16 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from datetime import datetime
 import pytz
+import logging
 
 app = FastAPI()
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s")
+
 
 def create_response(time: str) -> HTMLResponse:
+    logging.info("Creating HTML response")
     html_content = f"""
     <html>
         <head>
@@ -19,9 +25,15 @@ def create_response(time: str) -> HTMLResponse:
     """
     return HTMLResponse(content=html_content, status_code=200)
 
+
 @app.get("/")
 async def root():
-    moscow_tz = pytz.timezone('Europe/Moscow')
-    current_time = datetime.now(moscow_tz)
-    str_current_time = current_time.strftime("%H:%M:%S")
-    return create_response(str_current_time)
+    try:
+        logging.info("Request received at the root endpoint")
+        moscow_tz = pytz.timezone('Europe/Moscow')
+        current_time = datetime.now(moscow_tz)
+        str_current_time = current_time.strftime("%H:%M:%S")
+        return create_response(str_current_time)
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        return HTMLResponse(content="<p>Error</p>", status_code=500)
