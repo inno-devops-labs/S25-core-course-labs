26f6294ae72c005b51660b57f791c974e003ee41
Max <ibroden@yandex.ru>
2025-01-28 22:14:38 +0300

    [lab2] Use docker.

---
+++ app_go/README.md
@@ -0,0 +1,27 @@
+
+# Overview
+
+This service lets you monitor some data about linux kernel code repository, like
+
+- Number of commits
+- Time since the first release
+
+To run it locally, you may run
+
+```bash
+go run ./
+```
+
+With docker, you may try simple build:
+
+```bash
+docker build -t go_serv .
+docker run go_serv
+```
+
+or distroless build:
+
+```bash
+docker build -f distroless.dockerfile -t go_serv_dless .
+docker run go_serv_dless
+```
--- app_python/service/main.py
+++ app_python/service/main.py
@@ -6,6 +6,8 @@
  Author: Elon Max
 """
 
+import os
+import uvicorn
 from fastapi import FastAPI
 from routers import time_router
 
@@ -13,3 +15,13 @@ app = FastAPI()
 
 # Include the router from the time module
 app.include_router(time_router)
+
+if __name__ == "__main__":
+    print("Starting webserver...")
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 8080)),
+        log_level=os.getenv('LOG_LEVEL', "info"),
+        proxy_headers=True
+    )
