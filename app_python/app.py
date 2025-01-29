7d576b507854ea45406970ddf28b9d331789745c
Mikhail Voronin <mixail.voronin.2003-17@bk.ru>
2025-01-30 01:21:06 +0300

    Task 1 fixes

--- app_python/app.py
+++ app_python/app.py
@@ -1,11 +1,48 @@
+import pytz
 from flask import Flask
-from datetime import datetime, timezone, timedelta
+from datetime import datetime
 
 app = Flask(__name__)
 
+# Global variable with timezone
+TIME_ZONE = ""
+CONFIG_FILE = "config.txt"
+
+
+def get_timezone() -> str():
+    """
+    Loading timezone frome configuration file
+    """
+    try:
+        with open(CONFIG_FILE, "r") as config_file:
+            time_zone = config_file.read().strip()
+
+        return time_zone if time_zone else "Europe/Moscow"
+    except Exception:
+
+        # Default value
+        return "Europe/Moscow"
+
+
 @app.route("/")
-def time():
-    return f"{datetime.now(timezone(timedelta(hours=3))).strftime("%H:%M:%S")}"
+def current_time() -> str():
+    """
+    Returns the current time in timezone formatted as HH:MM:SS.
+    """
+    global TIME_ZONE
+
+    # Set up TIME_ZONE if it was not
+    if TIME_ZONE == "":
+        time_zone = get_timezone()
+
+        try:
+            TIME_ZONE = pytz.timezone(time_zone)
+        except Exception:
+            TIME_ZONE = pytz.timezone("Europe/Moscow")
+
+    time = datetime.now(TIME_ZONE)
+    return f"Current time in {TIME_ZONE} is {time.strftime("%H:%M:%S")}"
+
 
 if __name__ == "__main__":
     app.run()
