846715e109158e2ed5e095dc2b83706594a34051
MattWay224 <mattew04@mail.ru>
2025-01-29 11:54:42 +0300

    Bonus task via Java done

---
+++ app_java/src/main/java/com/example/mgntime/MgnTimeApplication.java
@@ -0,0 +1,11 @@
+package com.example.mgntime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgnTimeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MgnTimeApplication.class, args);
+	}
+}
