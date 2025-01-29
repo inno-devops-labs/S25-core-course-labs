686921416c6b86df3b65b0dde866d74796a8fae4
absorian <07221432bs@gmail.com>
2025-01-29 22:38:04 +0300

    [lab2] dockerize python web app

--- app_python/run.sh
+++ app_python/run.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-APP_ADDRESS="127.0.0.1"
-APP_PORT=8000
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+IMAGE_NAME="s25-devops-msk-time"
 
-HOT_RELOAD=1
+source ./.env
 
-uvicorn $([ "$HOT_RELOAD" == 1 ] && echo "--reload") src.main:app \
-    --workers 1 \
-    --host "$APP_ADDRESS" \
-    --port "$APP_PORT"
+if [ "$DOCKER_NO_SAVE" == 1 ]; then
+    docker image rmi "$IMAGE_NAME" > /dev/null;
+fi
+if [[ -z $(docker images | grep -w "$IMAGE_NAME") ]]; then
+    docker build -t "$IMAGE_NAME" "$SCRIPT_DIR";
+fi
+
+if [[ -z $(docker ps -a | grep -w "${IMAGE_NAME}_ct") ]]; then
+    docker run -i \
+        $([ "$DOCKER_NO_SAVE" == 1 ] && echo "--rm") \
+        $([ "$DEV" == 1 ] && 
+            echo "--mount type=bind,src=$SCRIPT_DIR/src,dst=/app/src";
+            echo "--mount type=bind,src=$SCRIPT_DIR/static,dst=/app/static";
+            echo "--mount type=bind,src=$SCRIPT_DIR/templates,dst=/app/templates";
+        ) \
+        --env-file .env \
+        -p "$APP_PORT:$APP_PORT" \
+        --name "${IMAGE_NAME}_ct" \
+        "$IMAGE_NAME";
+else
+    docker start -a "${IMAGE_NAME}_ct";
+fi
