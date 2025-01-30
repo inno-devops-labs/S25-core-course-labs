dea1e1341cd59645850c8a370704ece6b2796f82
absorian <07221432bs@gmail.com>
2025-01-30 20:52:21 +0300

    [lab2][bonus] dockerize cryptoclicker

---
+++ app_js/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+_term() { 
+  kill -INT "$child" 2>/dev/null
+}
+
+trap _term SIGINT
+
+npm run \
+  $([ "$DEV" == 1 ] && 
+      echo "dev" || echo "preview"
+  ) -- \
+  --host "$APP_HOST" \
+  --port "$APP_PORT" \
+  &
+
+child=$!
+wait "$child"
