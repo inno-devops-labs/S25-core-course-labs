60525eff735deaf2b777a6668eb08b17d2654a1b
darya <d.koncheva@innopolis.university>
2025-01-30 16:31:01 +0300

    add venv

---
+++ app_python/README.md
@@ -0,0 +1,6 @@
+# Moscow Time Web Application
+
+## About
+This is simple application with small API,
+that returns current time in Moscow and simple web page,
+which interacts with this API and display current time in Moscow.
--- app_python/main.py
+++ app_python/main.py
@@ -3,6 +3,7 @@ import pytz
 from flask import Flask, render_template
 
 TIMEZONE = "Europe/Moscow"
+TIME_FORMAT = "%Y.%m.%d %H:%M:%S"
 PAGE = "index.html"
 
 app = Flask(__name__)
@@ -12,7 +13,7 @@ def index():
     """
     Returns html page that displays time in Moscow
     """
-    time = datetime.now(pytz.timezone(TIMEZONE))
+    time = datetime.now(pytz.timezone(TIMEZONE)).strftime(TIME_FORMAT)
     return render_template(PAGE, time=time)
 
 if __name__ == "__main__":
