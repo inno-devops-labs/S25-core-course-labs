3d20b74aa75317cc55d3b3ff1a95deba46c3f5c0
Aleksandr Ryabov <s4.ryabow@yandex.ru>
2025-02-01 12:37:02 +0300

    Lab 3: fix black issues

--- app_python/tests/tests.py
+++ app_python/tests/tests.py
@@ -5,12 +5,14 @@ from bs4 import BeautifulSoup
 from flask.testing import FlaskClient
 from datetime import datetime, timezone, timedelta
 
+
 def test_availability(client: FlaskClient) -> None:
     """
     Pytest test availability
     """
     response = client.get("/")
     assert response.status_code == 200
+
 
 def test_html(client: FlaskClient) -> None:
     """
@@ -19,23 +21,25 @@ def test_html(client: FlaskClient) -> None:
     response = client.get("/").text
     assert "Moscow" in response, "Html is broken"
 
+
 def test_time(client: FlaskClient) -> None:
     """
     Pytest test Moscow time for correctness within the <p id="time" class="time"> tag
     """
     response = client.get("/").text
-    soup = BeautifulSoup(response, 'html.parser')
+    soup = BeautifulSoup(response, "html.parser")
     time_tag = soup.find("p", {"id": "time", "class": "time"})
     assert time_tag is not None, "Time tag is missing"
 
     zone = timezone(timedelta(hours=3))
     time = datetime.now(timezone.utc).astimezone(zone)
-    formatted_time = time.strftime('%H:%M:%S %d.%m.%Y')
+    formatted_time = time.strftime("%H:%M:%S %d.%m.%Y")
 
     assert formatted_time in time_tag.text, "Time is broken"
+
 
 @pytest.fixture
 def client():
     test_app = flask_app.create_flask_app()
-    test_app.config['TESTING'] = True
+    test_app.config["TESTING"] = True
     return test_app.test_client()
