cd1baacbaab0f140606bcf3f7fa6de77a677db29
dinaraparanid <dinaraparanid@gmail.com>
2025-02-01 13:49:12 +0300

    lab3

--- app_python/tests/test_app.py
+++ app_python/tests/test_app.py
@@ -1,5 +1,6 @@
 from app_python.app import app
 from datetime import datetime
+from time import sleep
 import pytz
 import unittest
 
@@ -18,12 +19,38 @@ class TimeAppTests(unittest.TestCase):
         Checks if home page is accessible and shows current time in Moscow
         """
 
+        self.check_web_page()
+
+    def test_continuous_time_display(self):
+        for i in range(0, 5):
+            self.check_web_page()
+            sleep(1)
+
+    def check_web_page(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
 
         moscow_tz = pytz.timezone("Europe/Moscow")
-        expected_time = datetime.now(moscow_tz).strftime("%H:%M:%S")
-        self.assertIn(expected_time, response.data.decode('utf-8'))
+        expected_time = datetime.now(moscow_tz).strftime("%H:%M:%S %d.%m.%Y")
+
+        self.assertEqual(
+            f'''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moscow Time</title>
+</head>
+<body>
+    <h1>Current Time in Moscow:</h1>
+    <h3>{expected_time}</h3>
+    <p>Refresh page to update the time</p>
+</body>
+</html>'''.strip(),
+            response.data.decode('utf-8').strip()
+        )
+
 
 
 if __name__ == '__main__':
