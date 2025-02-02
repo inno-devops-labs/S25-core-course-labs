9c351bef62299df2c760ef98f61701f3d3a8c917
Egor Meganov <e.meganov@innopolis.university>
2025-02-02 18:09:16 +0300

    lab3_draft2.2

--- app_python/tests/test_app.py
+++ app_python/tests/test_app.py
@@ -9,15 +9,15 @@ class FlaskTestCase(unittest.TestCase):
         self.app.testing = True
 
     def test_index(self):
-        response = self.app.get('/')
+        response = self.app.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'The current time in Moscow is:', response.data)
+        self.assertIn(b"The current time in Moscow is:", response.data)
 
     def test_get_time(self):
-        response = self.app.get('/time')
+        response = self.app.get("/time")
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'time', response.data)
+        self.assertIn(b"time", response.data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
