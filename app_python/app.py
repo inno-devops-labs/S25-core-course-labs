b30d1372c9978bbd3368a3a7954c3bbf1bb150eb
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2025-02-02 11:28:32 +0300

    refactor: apply black fixes

--- app_python/app.py
+++ app_python/app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 HOST = environ.get("HOST", "0.0.0.0")
 PORT = int(environ.get("PORT", "8080"))
 
+
 @app.route("/")
 def home():
     current_time = get_current_time_in_moscow()
--- app_python/tests/test_time_util.py
+++ app_python/tests/test_time_util.py
@@ -5,12 +5,14 @@ import pytest
 import pytz
 from utils.time_util import get_current_time_in_moscow
 
+
 @pytest.fixture
 def fixed_moscow_time():
     """Returns a fixed Moscow time for testing."""
     fixed_utc_time = datetime(2025, 2, 2, 12, 0, 0, tzinfo=pytz.utc)
     moscow_tz = pytz.timezone("Europe/Moscow")
     return fixed_utc_time.astimezone(moscow_tz).strftime("%Y-%m-%d %H:%M:%S")
+
 
 @patch("utils.time_util.datetime")
 def test_get_current_time_in_moscow(mock_datetime, fixed_moscow_time):
@@ -20,6 +22,7 @@ def test_get_current_time_in_moscow(mock_datetime, fixed_moscow_time):
     mock_datetime.strftime = datetime.strftime
 
     assert get_current_time_in_moscow() == fixed_moscow_time
+
 
 def test_output_format():
     """Test that the function returns time in the correct format."""
