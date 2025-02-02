dbead2d7d7624928c8fde399c9ad125e522d487c
Artur <arturmuxutdinov@gmail.com>
2025-02-02 23:30:07 +0300

    feat(project): structure
    initial project skeleton

--- app_python/tests/test_2_loader.py
+++ app_python/tests/test_2_loader.py
@@ -8,15 +8,14 @@ class TestAppConfiguration(unittest.TestCase):
     def setUp(self):
         self.app = initialize_app()
         self.app.config['SOME_ANOTHER_VAR'] = 'UNDEFINED'
-        self.app.config['PORT'] = 8000
-        self.app.config['DEBUG'] = True
+        self.app.config['PORT'] = 5000
+        self.app.config['DEBUG'] = False
         self.app.config['TESTING'] = True
         self.client = self.app.test_client()
-        print(self.app.config)
 
     def test_config_loading_from_env(self):
-        self.assertEqual(self.app.config['DEBUG'], True, "EXPECTED: DEBUG='True'")
-        self.assertEqual(self.app.config['PORT'], 8000, "EXPECTED: PORT='8000'")
+        self.assertEqual(self.app.config['DEBUG'], False, "EXPECTED: DEBUG='False'")
+        self.assertEqual(self.app.config['PORT'], 5000, "EXPECTED: PORT='5000'")
         self.assertEqual(self.app.config['TESTING'], True, "EXPECTED: TESTING=True")
         self.assertEqual(self.app.config['SOME_ANOTHER_VAR'], 'UNDEFINED', "EXPECTED: SOME_ANOTHER_VAR='UNDEFINED'")
 
