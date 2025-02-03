6e58fe96a08b783222b9027259e5b284117a22b9
Gleb Bugaev <b.gleb02@mail.ru>
2025-02-03 12:14:02 +0300

    Formatted app.py and test_app.py

--- app_python/app.py
+++ app_python/app.py
@@ -4,6 +4,7 @@ import pytz
 
 app = Flask(__name__)
 
+
 # Route for the root URL where moscow_time function will be executed
 @app.route('/')
 def moscow_time():
@@ -13,6 +14,7 @@ def moscow_time():
     moscow_time = datetime.now(moscow_tz).strftime('%d-%m-%Y    %H:%M:%S')
     # Returning the date and time to the web page
     return f"Current time in Moscow: {moscow_time}"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app_python/tests/test_app.py
+++ app_python/tests/test_app.py
@@ -3,6 +3,7 @@ from app import app
 from datetime import datetime
 import pytz
 
+
 class TestMoscowTimeApp(unittest.TestCase):
     # Create a test client for the Flask app
     def setUp(self):
@@ -16,55 +17,63 @@ class TestMoscowTimeApp(unittest.TestCase):
 
     # Check whether application returns a string with Moscow time
     def test_moscow_time_response_content(self):
-        """Test that the response contains the expected 'Current time in Moscow' string."""
+        """Test that the response contains the expected
+        'Current time in Moscow' string."""
         response = self.app.get('/')
         self.assertIn(b'Current time in Moscow', response.data)
 
-
     # Check whether application returns a Moscow time in supposed format
     def test_moscow_time_format(self):
-        """Test that the response includes the time in the correct format (DD-MM-YYYY HH:MM:SS)."""
+        """Test that the response includes the time in the correct
+        format (DD-MM-YYYY HH:MM:SS)."""
         response = self.app.get('/')
         response_text = response.data.decode('utf-8')
-        
+
         # Extract the time part from the response
         time_str = response_text.split(': ')[1]
-        
+
         # Define the expected date-time format
         expected_format = '%d-%m-%Y %H:%M:%S'
-        
+
         # Attempt to parse the time string using the expected format
         try:
             datetime.strptime(time_str, expected_format)
         except ValueError:
-            self.fail(f"Time string '{time_str}' does not match the expected format '{expected_format}'")
+            self.fail(f"Time string '{time_str}' does not match the expected" +
+                      f"format '{expected_format}'")
 
     # Check whether application returns a correct Moscow time
     def test_moscow_time_accuracy(self):
         """Test that the time displayed matches the current Moscow time."""
         response = self.app.get('/')
         response_text = response.data.decode('utf-8')
-        
+
         # Extract the time part from the response
         time_str = response_text.split(': ')[1]
-        
+
         # Parse the time string into the naive datetime object
-        moscow_time_in_response_naive = datetime.strptime(time_str, '%d-%m-%Y %H:%M:%S')
-        
+        moscow_time_in_response_naive = datetime.strptime(
+            time_str, '%d-%m-%Y %H:%M:%S')
+
         # Attach the Moscow timezone to the naive datetime object
         moscow_tz = pytz.timezone('Europe/Moscow')
-        moscow_time_in_response = moscow_tz.localize(moscow_time_in_response_naive)
-        
+        moscow_time_in_response = moscow_tz.localize(
+            moscow_time_in_response_naive)
+
         # Get the current Moscow time
         current_moscow_time = datetime.now(moscow_tz)
-        
+
         # Allow a small inaccuracy due to computation and comparison overhead
         allowed_margin = 5
-        
+
         # Compare the times
-        time_difference = abs((current_moscow_time - moscow_time_in_response).total_seconds())
+        time_difference = abs((current_moscow_time -
+                               moscow_time_in_response).total_seconds())
         self.assertLessEqual(time_difference, allowed_margin, 
-                             f"Displayed time '{moscow_time_in_response}' does not match current Moscow time '{current_moscow_time}'")
+                             f"Displayed time '{moscow_time_in_response}'" +
+                             "does not match current Moscow time" +
+                             f"'{current_moscow_time}'")
+
 
 if __name__ == '__main__':
     unittest.main()
