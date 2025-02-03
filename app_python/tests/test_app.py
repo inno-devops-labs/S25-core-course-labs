3455851edc002e2177de29494fc23a13ecb1c6e1
WellNotWell <olesya.gredyushko4956@gmail.com>
2025-02-04 00:32:25 +0300

    lab 3_1.1

--- app_python/tests/test_app.py
+++ app_python/tests/test_app.py
@@ -12,12 +12,20 @@ class AppTests(unittest.TestCase):
         self.app.testing = True
 
     def test_moscow_time_calc(self):
-        expected_msc_time = datetime.datetime.now(pytz.timezone("Europe/Moscow"))
+        expected_msc_time = (datetime.datetime
+                             .now(pytz.timezone("Europe/Moscow")))
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
 
-        displayed_time = response.data.decode('utf-8').split("Moscow Time: ")[1].split("</h1>")[0].strip()
-        self.assertEqual(displayed_time, expected_msc_time.strftime("%Y-%m-%d %H:%M:%S"))
+        displayed_time = (response
+                          .data
+                          .decode('utf-8')
+                          .split("Moscow Time: ")[1]
+                          .split("</h1>")[0]
+                          .strip()
+                          )
+        self.assertEqual(displayed_time,
+                         expected_msc_time.strftime("%Y-%m-%d %H:%M:%S"))
 
     def test_home_page_rendering(self):
         response = self.app.get('/')
