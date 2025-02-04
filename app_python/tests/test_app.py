45d3e7e077cf0abbea920871625441829eb6aa41
Angelika2707 <53828140+Angelika2707@users.noreply.github.com>
2025-02-04 21:21:37 +0300

    lab3

--- app_python/tests/test_app.py
+++ app_python/tests/test_app.py
@@ -17,9 +17,14 @@ class TestFlaskApp(unittest.TestCase):
 
     def test_moscow_time_correctness(self):
         moscow_time_zone = pytz.timezone("Europe/Moscow")
-        expected_time = datetime.now(moscow_time_zone).strftime("%Y-%m-%d %H:%M:%S")
+        expected_time = (datetime.now(moscow_time_zone)
+                         .strftime("%Y-%m-%d %H:%M:%S"))
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
-        actual_time = response.data.decode('utf-8').split("<p>")[1].split("</p>")[0].strip()
+        actual_time = (response.data
+                       .decode('utf-8')
+                       .split("<p>")[1]
+                       .split("</p>")[0]
+                       .strip())
         self.assertEqual(actual_time, expected_time)
