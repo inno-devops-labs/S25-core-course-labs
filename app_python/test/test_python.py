1c2746ffffc527207b22a2596a5f7a969694699d
MattWay224 <win220404@mail.ru>
2025-02-04 18:32:04 +0300

    init ci commit

--- app_python/test/test_python.py
+++ app_python/test/test_python.py
@@ -11,7 +11,7 @@ def client():
         yield client
 
 
-def test_moscow_time(client):
+def test_dynamuc_time(client):
     moscow = timezone('Europe/Moscow')
     expected_time = datetime.now(moscow).strftime('%Y-%m-%d %H:%M:%S')
 
@@ -19,3 +19,14 @@ def test_moscow_time(client):
 
     assert response.status_code == 200
     assert expected_time[:16] in response.data.decode('utf-8')
+
+def test_static_time(mock_datetime, client):
+    moscow = timezone('Europe/Moscow')
+    fixed_time = datetime(2024, 2, 4, 12, 0, 0, tzinfo=moscow)
+
+    mock_datetime.now.return_value = fixed_time
+
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert fixed_time in response.data.decode('utf-8')
