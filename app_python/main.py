95e93638cfbe2300f4d633ca74e6af53b9a185c3
Kazan-Strelnikova <strelnikova.kira354@gmail.com>
2025-02-04 11:45:37 +0300

    lint errors fix

--- app_python/main.py
+++ app_python/main.py
@@ -4,10 +4,13 @@ import pytz
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def current_time():
     moscow_time = datetime.now(pytz.timezone('Europe/Moscow'))
-    return f"Current time in Moscow: {moscow_time.strftime('%Y-%m-%d %H:%M:%S')}"
+    return f"Current time in Moscow: \
+        {moscow_time.strftime('%Y-%m-%d %H:%M:%S')}"
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
--- app_python/tests/test_main.py
+++ app_python/tests/test_main.py
@@ -3,12 +3,15 @@ from app_python.main import current_time
 from datetime import datetime
 import pytz
 
+
 class TestCurrentTime(unittest.TestCase):
     def test_moscow_time_format(self):
         moscow_time = datetime.now(pytz.timezone('Europe/Moscow'))
         result = current_time()  # Simulate calling the route
-        expected = f"Current time in Moscow: {moscow_time.strftime('%Y-%m-%d %H:%M:%S')}"
+        expected = f"Current time in Moscow: \
+            {moscow_time.strftime('%Y-%m-%d %H:%M:%S')}"
         self.assertEqual(result, expected)
+
 
 if __name__ == "__main__":
     unittest.main()
