561b6af66a3356fe87e220cf4eddd135c7e40c68
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2025-02-04 13:44:15 +0300

    feat: add tests

--- app_typescript/vite.config.ts
+++ app_typescript/vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
@@ -13,5 +16,10 @@ export default defineConfig({
         strictPort: true,
         host: true,
         origin: '0.0.0.0',
+    },
+    test: {
+        environment: 'jsdom',
+        setupFiles: ['./src/tests/setup.ts'],
+        globals: true,
     },
 });
