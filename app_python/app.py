5ae4ba45de1ec703a10bc2f958806b74151759f1
Ilsiia <i.nasibullina@innopolis.university>
2025-02-06 00:17:03 +0300

    Ilsiia Nasibullina - Lab3

--- app_python/app.py
+++ app_python/app.py
@@ -12,6 +12,6 @@ def getCurrentTime():
     currentTime = datetime.now(moscowTimeZone).strftime("%H:%M:%S")
     return render_template("index.html", time=currentTime)
 
-
+# Start the application
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
