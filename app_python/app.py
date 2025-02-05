90bf277457bed8101df86decd8192a11b5099233
MarketerKA <karhipov606@gmail.com>
2025-02-05 20:42:23 +0300

    Lab-3 Updt: Part 6 remove debug mode

--- app_python/app.py
+++ app_python/app.py
@@ -14,4 +14,4 @@ def index():
     return render_template('index.html', time=time_str, date=date_str)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000)
