550917a409a0d729eb5916439766c79f2cb59888
Muhibullo <mkhujabekov44@gmail.com>
2025-02-06 02:22:40 +0300

    flake8 fix

--- app_python/app.py
+++ app_python/app.py
@@ -4,12 +4,14 @@ import pytz
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    moscow_tz = pytz.timezone('Europe/Moscow')
-    moscow_time = datetime.now(moscow_tz)
-    current_time = moscow_time.strftime('%Y-%m-%d %H:%M:%S')
-    return render_template('index.html', current_time=current_time)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/")
+def home():
+    moscow_tz = pytz.timezone("Europe/Moscow")
+    moscow_time = datetime.now(moscow_tz)
+    current_time = moscow_time.strftime("%Y-%m-%d %H:%M:%S")
+    return render_template("index.html", current_time=current_time)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
