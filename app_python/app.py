4a48eb1a8e46d7d6aa1c2e3a15781d3275b513d7
Ali Hamdan <alihamdan@Alis-MacBook-Air-2.local>
2025-02-05 22:52:57 +0300

    updating

--- app_python/app.py
+++ app_python/app.py
@@ -4,11 +4,16 @@ import pytz
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def show_time():
     moscow_tz = pytz.timezone('Europe/Moscow')
     current_time = datetime.now(moscow_tz)
-    return render_template('index.html', time=current_time.strftime('%Y-%m-%d %H:%M:%S'))
+    return render_template(
+        'index.html',
+        time=current_time.strftime('%Y-%m-%d %H:%M:%S')
+    )
+
 
 if __name__ == '__main__':
     app.run()
