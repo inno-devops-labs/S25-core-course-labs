71fa8322dacdd4a21fa760e55a46998419ebccc1
Roman Pogrebnyak <grabberisonfire@gmail.com>
2025-02-06 02:58:28 +0300

    fix 5

--- app_python/app/main.py
+++ app_python/app/main.py
@@ -11,24 +11,29 @@ app = FastAPI()
 
 current_dir = Path(__file__).parent
 templates = Jinja2Templates(directory=str(current_dir.parent / "templates"))
-app.mount("/static", StaticFiles(directory=str(current_dir.parent / "static")), name="static")
+app.mount("/static", StaticFiles(directory=str(current_dir.parent / "static")),
+          name="static")
+
 
 class TimeData(BaseModel):
     time: str
     timestamp: float
 
+
 @app.get("/api/moscow_time")
 async def get_moscow_time() -> TimeData:
     moscow_tz = pytz.timezone('Europe/Moscow')
     return TimeData(
-        time= datetime.now(moscow_tz).strftime("%Y-%m-%d %H:%M:%S"),
-        timestamp= datetime.now(moscow_tz).timestamp()
+        time=datetime.now(moscow_tz).strftime("%Y-%m-%d %H:%M:%S"),
+        timestamp=datetime.now(moscow_tz).timestamp()
     )
+
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request) -> HTMLResponse:
     current_time = await get_moscow_time()
-    return templates.TemplateResponse(request,
+    return templates.TemplateResponse(
+        request,
         "index.html",
         {"current_time": current_time}
     )
--- app_python/tests/test_app.py
+++ app_python/tests/test_app.py
@@ -5,10 +5,11 @@ from fastapi.testclient import TestClient
 
 from app.main import app
 
+
 class TestMoscowTimeAPI(unittest.TestCase):
     def setUp(self) -> None:
         self.client = TestClient(app)
-        
+
     def test_root_route(self) -> None:
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
@@ -38,6 +39,7 @@ class TestMoscowTimeAPI(unittest.TestCase):
         response = self.client.get("/api/moscow_time")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()["time"], "2024-01-15 15:00:00")
+
 
 if __name__ == '__main__':
     unittest.main()
