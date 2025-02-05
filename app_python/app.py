895c10c1288fcc8a8dc29e09b1a85d87c6c73ead
Kashapov Shamil <favelanky@gmail.com>
2025-02-05 19:57:12 +0300

    fix localhost

--- app_python/app.py
+++ app_python/app.py
@@ -10,5 +10,4 @@ def show_time():
     return f"<h1>Current Time in Moscow: {moscow_time.strftime('%Y-%m-%d %H:%M:%S')}</h1>"
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run(host='0.0.0.0', port=5000)
