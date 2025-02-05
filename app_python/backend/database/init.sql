adecb7fe5d35c6cc4ba66f41eefa1b4ad6f60f64
JulyMartynova <137276455+JulyMartynova@users.noreply.github.com>
2025-02-05 23:10:51 +0300

    Update init.sql

--- app_python/backend/database/init.sql
+++ app_python/backend/database/init.sql
@@ -10,7 +10,7 @@ CREATE TABLE zone (
     id SERIAL NOT NULL,
      name VARCHAR(256),
     timezone VARCHAR(20),
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 );
 INSERT INTO zone (name, timezone) VALUES ('London', 'Europe/London');
 INSERT INTO zone (name, timezone) VALUES ('Moscow', 'Europe/Moscow');
