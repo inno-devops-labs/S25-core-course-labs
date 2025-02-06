8cd2c0432e72762247c00ccfaaf8ca0215b17be9
Nika <137218793+NikaChek@users.noreply.github.com>
2025-02-06 13:54:27 +0300

    Update main.tf

--- terraform/yacloud/main.tf
+++ terraform/yacloud/main.tf
@@ -1,32 +1,30 @@
 terraform {
   required_providers {
     yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "~> 0.89"
+      source = "yandex-cloud/yandex"
     }
   }
 }
 
 provider "yandex" {
-  token     = var.yc_token  
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
+  token     = var.yc_token
+  cloud_id  = var.yc_cloud_id
+  folder_id = var.yc_folder_id
   zone      = "ru-central1-a"
 }
 
-resource "yandex_compute_instance" "vm_instance" {
-  name        = "terraform-vm"
-  platform_id = "standard-v1"
+resource "yandex_compute_instance" "vm1" {
+  name = "my-vm"
+  zone = "ru-central1-a"
 
   resources {
-    cores         = 2
-    memory        = 2
-    core_fraction = 20
+    cores  = 2
+    memory = 2
   }
-  
+
   boot_disk {
     initialize_params {
-      image_id = var.image_id 
+      image_id = "fd80bm0rh4rkepi5ksdi"
     }
   }
 
@@ -34,35 +32,8 @@ resource "yandex_compute_instance" "vm_instance" {
     subnet_id = var.subnet_id
     nat       = true
   }
-}
 
-variable "yc_token" {
-  description = "API Token for Yandex Cloud"
-  type        = string
-}
-
-variable "cloud_id" {
-  description = "ID of the Yandex Cloud"
-  type        = string
-}
-
-variable "folder_id" {
-  description = "ID of the Folder in Yandex Cloud"
-  type        = string
-}
-
-variable "image_id" {
-  description = "ID of the Yandex Cloud Image"
-  type        = string
-  default     = "fd80bm0rh4rkepi5ksdi" 
-}
-
-variable "subnet_id" {
-  description = "Subnet ID for Yandex Cloud"
-  type        = string
-}
-
-output "vm_public_ip" {
-  description = "Public IP of the created virtual machine"
-  value       = yandex_compute_instance.vm_instance.network_interface[0].nat_ip_address
+  metadata = {
+    ssh-keys = "ubuntu:${var.ssh_public_key}"
+  }
 }
