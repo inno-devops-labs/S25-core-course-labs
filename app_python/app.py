097b6950abb5a5208f612ef1740908b662ef21b9
AlexeyKureykin <59525932+AlexeyKureykin@users.noreply.github.com>
2025-02-06 05:38:31 +0300

    Update app.py

--- app_python/app.py
+++ app_python/app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from datetime import datetime
 import pytz
 
-
 # Flask initialization
 app = Flask(__name__)
 
@@ -13,6 +12,7 @@ def check_time():
     msc_tz = pytz.timezone("Europe/Moscow")
     current_time = datetime.now(msc_tz).strftime("%Y-%m-%d %H:%M:%S")
     return f"<h1>Current time in Moscow: {current_time}</h1>"
+    
 
 # Starting the app
 if __name__ == "__main__":
