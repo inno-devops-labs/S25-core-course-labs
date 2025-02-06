de90593873efa44ba872b07e0f51c571ed208a1e
TikhonKhod <tikhon0710@gmail.com>
2025-02-06 22:46:38 +0300

    lab3.5

--- app_python/test/test_app.py
+++ app_python/test/test_app.py
@@ -1,13 +1,13 @@
-import sys
-import os
+# import sys
+# import os
 
-sys.path.insert(
-    0,
-    os.path.abspath(
-        os.path.join(
-            os.path.dirname(__file__),
-            "..",
-            "..")))
+# sys.path.insert(
+#     0,
+#     os.path.abspath(
+#         os.path.join(
+#             os.path.dirname(__file__),
+#             "..",
+#             "..")))
 
 import re
 import pytz
