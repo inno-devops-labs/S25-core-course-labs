43ae38967454d3d03dbb859cfae3f285d207bd59
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2025-02-06 14:30:33 +0300

    fix: wrong path

--- terraform/github-teams/outputs.tf
+++ terraform/github-teams/outputs.tf
@@ -2,7 +2,7 @@ output "repositories" {
   value = [
     {
       name           = github_repository.repo.name
-      default_branch = github_branch_default.repo_main.branch
+      default_branch = github_branch_default.main.branch
       id             = github_repository.repo.id
     },
   ]
