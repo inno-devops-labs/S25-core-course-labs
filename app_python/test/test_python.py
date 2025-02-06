ebbfa6b5ea3498bf3290b13c7f77d607abb610b9
MattWay224 <mattew04@mail.ru>
2025-02-06 14:14:36 +0300

    correcting tests

--- app_python/test/test_python.py
+++ app_python/test/test_python.py
@@ -3,13 +3,6 @@ from pytz import timezone
 from datetime import datetime
 from unittest.mock import patch
 from app_python.run import app
-import sys
-import os
-
-sys.path.insert(0,
-                os.path.abspath(
-                    os.path.join(
-                        os.path.dirname(__file__), '../')))
 
 
 @pytest.fixture
