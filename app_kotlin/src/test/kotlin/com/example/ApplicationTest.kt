56d9401623f5e4fcf3abfddf055eaea7f3c28290
emapfff <dvlemil_777@mail.ru>
2025-02-06 18:50:44 +0300

    complete lab3

--- app_kotlin/src/test/kotlin/com/example/ApplicationTest.kt
+++ app_kotlin/src/test/kotlin/com/example/ApplicationTest.kt
@@ -16,14 +16,8 @@ class MainTest : DescribeSpec({
                     response.status() shouldBe HttpStatusCode.OK
                     val content = response.content
                     content shouldContain "Current Time in Moscow:"
-
-                    // Extract the date portion from the response (assuming it's the rest of the content)
                     val dateString = content?.replace("Current Time in Moscow:", "")?.trim()
-
-                    // Add a regex pattern for the date format (yyyy-MM-dd HH:mm:ss)
                     val dateRegex = """\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}""".toRegex()
-
-                    // Check if the date string matches the regex pattern
                     dateString shouldMatch dateRegex
                 }
             }
