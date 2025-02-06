88393754ff224c9b47d78b998b14a6588d21009a
Galia <g1l4a>
2025-02-06 12:38:44 +0300

    fixed

--- app_python/time_app/time_app/tests.py
+++ app_python/time_app/time_app/tests.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import pytz
 import re
 
+
 class MoscowTimeViewTests(TestCase):
     """
     Test case for the Moscow time view.
@@ -21,7 +22,6 @@ class MoscowTimeViewTests(TestCase):
         Test if the response contains the correct Moscow time format.
         """
         response = self.client.get(reverse('moscow_time'))
-        
         match = re.search(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})", response.content.decode())
         self.assertIsNotNone(match, "Response does not contain a valid timestamp")
 
@@ -30,16 +30,12 @@ class MoscowTimeViewTests(TestCase):
         Test if the time displayed is close to the actual Moscow time.
         """
         response = self.client.get(reverse('moscow_time'))
-        
         match = re.search(r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})", response.content.decode())
         self.assertIsNotNone(match, "Response does not contain a valid timestamp")
-
         response_time_str = match.group(1)
         response_time = datetime.strptime(response_time_str, "%Y-%m-%d %H:%M:%S")
-        
         moscow_timezone = pytz.timezone("Europe/Moscow")
         actual_moscow_time = datetime.now(moscow_timezone).strftime("%Y-%m-%d %H:%M:%S")
         actual_moscow_time_obj = datetime.strptime(actual_moscow_time, "%Y-%m-%d %H:%M:%S")
-
         time_difference = abs((actual_moscow_time_obj - response_time).total_seconds())
         self.assertLessEqual(time_difference, 5, "Moscow time in response is incorrect")
--- app_python/time_app/time_app/views.py
+++ app_python/time_app/time_app/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from datetime import datetime
 import pytz
 
+
 def moscow_time(request):
     moscow_timezone = pytz.timezone("Europe/Moscow")
     current_time = datetime.now(moscow_timezone).strftime("%Y-%m-%d %H:%M:%S")
