ca931a897ba70dbde0891cd2390419b454bfe88f
Aleksei Kureikin <a.kureikin@innopolis.university>
2025-02-06 08:57:45 +0300

    Adding variable for container's name

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -1,11 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.0"
+    }
+  }
+}
+
 provider "docker" {}
+
+variable "container_name" {
+  default = "my-nginx-container"
+}
 
 resource "docker_image" "nginx" {
   name = "nginx:latest"
 }
 
 resource "docker_container" "nginx" {
-  name  = "terraform-nginx"
+  name  = var.container_name
   image = docker_image.nginx.image_id
   ports {
     internal = 80
