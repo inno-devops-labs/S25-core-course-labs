bd4a3c64fd1f97a35d50d1ba1c7d94e6714feb4f
Aymen Daassi <aymounadaassi@gmail.com>
2025-02-06 22:55:53 +0300

    fix linting

--- app_python/tests/test_main.py
+++ app_python/tests/test_main.py
@@ -1,12 +1,17 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import pytest
 import re
 from datetime import datetime
 import pytz
 from main import app
+
+
+sys.path.insert(0, 
+                os.path.abspath(
+                os.path.join(
+                os.path.dirname(__file__),
+                '..')))
 
 
 @pytest.fixture
