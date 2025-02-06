b61f90468c093b4251176094408a4d4b4343868a
dpttk <github@dpttk.ru>
2025-02-06 14:03:22 +0300

    Lab-04

---
+++ terraform/github/outputs.tf
@@ -0,0 +1,7 @@
+output "repository_url" {
+  value       = github_repository.S25-core-course-labs.html_url
+}
+
+output "repository_id" {
+  value       = github_repository.S25-core-course-labs.node_id
+} 
---
+++ terraform/github/variables.tf
@@ -0,0 +1,9 @@
+variable "github_token" {
+  type        = string
+  sensitive   = true
+}
+
+variable "repository_name" {
+  type        = string
+  default     = "S25-core-course-labs"
+} 
