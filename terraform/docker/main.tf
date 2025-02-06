032c2dc235310fff897a3fc2155472d88735a071
Muhibullo <mkhujabekov44@gmail.com>
2025-02-06 21:40:28 +0300

    update

--- terraform/docker/main.tf
+++ terraform/docker/main.tf
@@ -24,9 +24,13 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "tutorial"
+  name  = var.container_name
   ports {
     internal = 80
     external = 8000
   }
 }
+
+
+
+
