6ed9391851f6c7838161a940ffef5154d2aeef20
MattWay224 <mattew04@mail.ru>
2025-02-06 13:48:45 +0300

    Correcting tests

--- app_python/test/test_python.py
+++ app_python/test/test_python.py
@@ -1,12 +1,13 @@
 import pytest
 from pytz import timezone
 from datetime import datetime
-
-from app_python.app import app
+from unittest.mock import patch
+from app_python.run import app
 
 
 @pytest.fixture
 def client():
+    app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
@@ -21,13 +22,15 @@ def test_dynamuc_time(client):
     assert expected_time[:16] in response.data.decode('utf-8')
 
 
-def test_static_time(mock_datetime, client):
+def test_static_time(monkeypatch, client):
     moscow = timezone('Europe/Moscow')
     fixed_time = datetime(2024, 2, 4, 12, 0, 0, tzinfo=moscow)
 
-    mock_datetime.now.return_value = fixed_time
+    with patch("app_python.run.datetime") as mock_datetime:
+        mock_datetime.now.return_value = fixed_time
+        mock_datetime.strftime = datetime.strftime
 
-    response = client.get('/')
+        response = client.get('/')
 
-    assert response.status_code == 200
-    assert fixed_time in response.data.decode('utf-8')
+        assert response.status_code == 200
+        assert fixed_time.strftime('%Y-%m-%d %H:%M:%S') in response.data.decode('utf-8')
