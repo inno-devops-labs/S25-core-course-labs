01fc9a245f3e13364a633741f821efdf6e0ecfc5
Magelon <lishchenko.ivan@gmail.com>
2025-02-06 04:50:07 +0300

    7th change to the CI

--- app_python/tests/conftest.py
+++ app_python/tests/conftest.py
@@ -1,15 +1,17 @@
 import sys
 import pytest
 
-sys.path.append('..\\app_python')
+# sys.path.append('..\\app_python')
 
-import app as app_python_app
+# import app as app_python_app
 """
 Primarily taken from https://flask.palletsprojects.com/en/stable/tutorial/tests/ and
 then changed to be applicable to this project, as it is simpler than their example.
 """
 @pytest.fixture
 def app():
+    sys.path.append('..\\app_python')
+    import app as app_python_app
     this_app = app_python_app.app
     yield this_app
 
