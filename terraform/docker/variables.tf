51abe798679060bf859566f44710725a45495283
bakinsa <bakinasa@gmail.com>
2025-02-06 12:42:44 +0300

    feat(lab4): configure terraform for docker and yandex cloud

---
+++ terraform/docker/variables.tf
@@ -0,0 +1,19 @@
+variable "container_name" {
+  description = "Name of the Docker container"
+  default     = "my-nginx-container"
+}
+
+variable "image_name" {
+  description = "Docker image name"
+  default     = "nginx:latest"
+}
+
+variable "internal_port" {
+  description = "Internal port of the container"
+  default     = 80
+}
+
+variable "external_port" {
+  description = "External port of the container"
+  default     = 8080
+}
