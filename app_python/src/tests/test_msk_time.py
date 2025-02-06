06ed8c975ad70763ceae1a483ea92f777ada308b
Ismagil <absoriann@gmail.com>
2025-02-06 12:04:54 +0300

    [lab3] Lint the code

--- app_python/src/tests/test_msk_time.py
+++ app_python/src/tests/test_msk_time.py
@@ -5,10 +5,11 @@ import pytz
 
 client = TestClient(app)
 
+
 def test_get_msk_time():
     response = client.get("/")
     assert response.status_code == 200
 
-    current_date = datetime.datetime.now(pytz.timezone('Europe/Moscow'))
-    
+    current_date = datetime.datetime.now(pytz.timezone("Europe/Moscow"))
+
     assert current_date.strftime("%H:%M") in response.text
