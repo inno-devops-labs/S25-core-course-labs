ff62febabed2ccfee9abcb99b4bd409b89ecfb21
Aymen Daassi <aymounadaassi@gmail.com>
2025-02-06 22:09:02 +0300

    add eslint config

--- app_nodejs/eslint.config.js
+++ app_nodejs/eslint.config.js
@@ -1,14 +1,9 @@
-module.exports = {
-    env: {
-      node: true,
-      es2021: true,
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    rules: {
-    },
-  };
-  
+// eslint.config.js
+module.exports = [
+    {
+        rules: {
+            semi: "error",
+            "prefer-const": "error"
+        }
+    }
+];
