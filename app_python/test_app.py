18fc13512dbbd1b5846cee4a0eff8b693c2f3b30
Ali Hamdan <ali12hamdan2001@gmail.com>
2025-02-06 23:44:50 +0300

    Update test_app.py
    finalizing linter

--- app_python/test_app.py
+++ app_python/test_app.py
@@ -10,10 +10,12 @@ def client():
     with app.test_client() as client:
         yield client
 
+
 def test_index_route(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b'Current Time in Moscow' in response.data
+
 
 def test_time_format(client):
     moscow_tz = pytz.timezone('Europe/Moscow')
