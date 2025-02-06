b29e4871a0fcd81eea755c5d128119b707855f42
Magelon <lishchenko.ivan@gmail.com>
2025-02-06 04:53:02 +0300

    9th change to the CI

--- app_python/tests/conftest.py
+++ app_python/tests/conftest.py
@@ -10,8 +10,8 @@ then changed to be applicable to this project, as it is simpler than their examp
 """
 @pytest.fixture
 def app():
-    print(sys.path)
-    sys.path.append('..\\app_python')
+    # print(sys.path)
+    sys.path.append('..\\..\\app_python')
     import app as app_python_app
     this_app = app_python_app.app
     yield this_app
