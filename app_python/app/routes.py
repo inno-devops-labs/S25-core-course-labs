4f5734ee5de26b1b863609507509233ae5978735
Artur <arturmuxutdinov@gmail.com>
2025-02-13 21:13:11 +0300

    feat(project): structure
    initial project skeleton

--- app_python/app/routes.py
+++ app_python/app/routes.py
@@ -1,7 +1,13 @@
+import logging
 from datetime import datetime
+from logging import FileHandler
 
 import pytz
 from flask import current_app as app, render_template
+
+file_handler = FileHandler('app.log')
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
 
 
 @app.route('/')
@@ -12,5 +18,7 @@ def index():
     """
     moscow_tz = pytz.timezone('Europe/Moscow')
     moscow_time = datetime.now(moscow_tz).strftime('%H:%M:%S')
+
+    app.logger.info("Accessed / endpoint: %s", f"Moscow time is: {moscow_time}")
 
     return render_template('index.html', moscow_time=moscow_time)
