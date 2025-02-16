9b29865be6530fa5c46421e87de08d44156f4508
Efim Puzhalov <e.puzhalov@innopolis.universtiy>
2025-02-16 23:20:12 +0300

    fix: lint

--- app_python/app.py
+++ app_python/app.py
@@ -15,10 +15,12 @@ def moscow_time():
     current_time = datetime.now(moscow_tz).strftime("%Y-%m-%d %H:%M:%S")
     return {"Current time in Moscow": current_time}
 
+
 @app.get("/metrics")
 def metrics():
     # Generate Prometheus metrics in the text format
     return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
+
 
 if __name__ == "__main__":
     import uvicorn
