16876c70c4a1ed9f26c31e2565a8579f1a69b92a
paranid5 <dinaraparanid@gmail.com>
2025-02-16 14:46:06 +0300

    lab8

--- app_python/app.py
+++ app_python/app.py
@@ -17,8 +17,7 @@ def home():
     """
     moscow_tz = pytz.timezone('Europe/Moscow')
     moscow_time = datetime.now(moscow_tz).strftime('%H:%M:%S %d.%m.%Y')
-    print(moscow_time)
-    return render_template('index.html', moscow_time='debug=False')
+    return render_template('index.html', moscow_time=moscow_time)
 
 
 if __name__ == '__main__':
