3eafc5f9a687f5844ee890e452b0060f5a289c4e
Gleb Bugaev <b.gleb02@mail.ru>
2025-02-16 16:19:06 +0300

    Added logs to app.py

--- app_python/app.py
+++ app_python/app.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 from datetime import datetime
 import pytz
+import logging
 
 app = Flask(__name__)
+
+# Configure logging to output to stdout
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()]
+)
+
+# Create a logger instance
+logger = logging.getLogger(__name__)
 
 
 # Route for the root URL where moscow_time function will be executed
@@ -12,6 +23,11 @@ def moscow_time():
     moscow_tz = pytz.timezone('Europe/Moscow')
     # Formatting the date and time
     moscow_time = datetime.now(moscow_tz).strftime('%d-%m-%Y    %H:%M:%S')
+
+    # Log the request details
+    logger.info(f"Request received from {request.remote_addr}" +
+                f" at {moscow_time}")
+
     # Returning the date and time to the web page
     return f"Current time in Moscow: {moscow_time}"
 
