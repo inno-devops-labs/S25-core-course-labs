d5a822215d6e38f78582c6c424eee0fa52051ba6
Ilsiia <i.nasibullina@innopolis.university>
2025-02-19 22:06:08 +0300

    Ilsiia Nasibullina -Lab 8

--- app_python/app.py
+++ app_python/app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 REQUEST_COUNT = Counter("app_requests_total", "Total number of requests")
 CURRENT_TIME = Gauge("app_current_time", "Current Moscow time in seconds")
 
+
 @app.route("/")
 def getCurrentTime():
     REQUEST_COUNT.inc()  # Increment request count
@@ -21,10 +22,11 @@ def getCurrentTime():
 
     return render_template("index.html", time=currentTime)
 
+
 @app.route("/metrics")
 def metrics():
-    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
+    return generate_latest(), 200, {"Content-Type": CONTENT_TYPE_LATEST}
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
-
