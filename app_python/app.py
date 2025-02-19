a032ce7869484fd99ec898315ba9493a05610e8d
saleemasekrea000 <s.asekrea@innopolis.university>
2025-02-19 15:47:14 +0300

    integrate Prometheus metrics tracking

--- app_python/app.py
+++ app_python/app.py
@@ -2,10 +2,15 @@
 Main application file. Contains the route information and necessary setups.
 """
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from fastapi.responses import HTMLResponse
 
 from app_python.app_service import get_time
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+
+REQUEST_COUNT = Counter('http_requests_total', 'Total number of HTTP requests', ['method', 'endpoint'])
+
+
 
 app = FastAPI()
 
@@ -15,8 +20,13 @@ def show_time(request: Request):
     """
     Shows the current time in Moscow.
     """
+    REQUEST_COUNT.labels(method=request.method, endpoint=request.url.path).inc()  # Increment the counter
+
     return get_time(request)
 
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
