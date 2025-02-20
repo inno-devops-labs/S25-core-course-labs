c422a411997fb96cf2d391c78ea4ed48d534afc2
Kazan-Strelnikova <strelnikova.kira354@gmail.com>
2025-02-20 09:00:40 +0300

    lab8

--- app_javascript/server.js
+++ app_javascript/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const path = require('path');
 const winston = require('winston');
+const client = require('prom-client');
 
 const app = express();
 const port = 3000;
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ timeout: 5000 });
+
+const httpRequestsTotal = new client.Counter({
+  name: 'app_javascript_requests_total',
+  help: 'Total requests to the application'
+});
 
 // Set up Winston logger
 const logDir = path.join(__dirname, 'logs');
@@ -23,7 +32,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Optional: a route to send a custom message if needed
 app.get('/', (req, res) => {
   logger.info('Home page requested');
+  httpRequestsTotal.inc();
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
 });
 
 // Start the server only if this module is run directly
--- app_python/main.py
+++ app_python/main.py
@@ -2,8 +2,11 @@ from flask import Flask
 from datetime import datetime
 import pytz
 import logging
+from prometheus_client import CONTENT_TYPE_LATEST, generate_latest, Counter, start_http_server
 
 app = Flask(__name__)
+
+REQUEST_COUNT = Counter('app_python_requests_total', 'Total requests to the application')
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -16,10 +19,19 @@ def current_time():
     
     # Log the current time request
     logger.info(f"Current time requested: {time_str}")
+    # Increment the request counter
+    REQUEST_COUNT.inc()
     
     return f"Current time in Moscow: {time_str}"
+
+@app.route('/metrics')
+def metrics():
+    # Return the current metrics as a response
+     return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
 
 if __name__ == '__main__':
     # Log when the server starts
     logger.info("Starting Flask server...")
+    # Expose metrics at port 8000
+    start_http_server(8000)
     app.run(host="0.0.0.0", port=5000)
