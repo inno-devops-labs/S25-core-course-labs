c77450be6c3b19e46010a762393dce041075848a
Kazan-Strelnikova <strelnikova.kira354@gmail.com>
2025-02-20 09:26:56 +0300

    test fix

--- app_python/tests/test_main.py
+++ app_python/tests/test_main.py
@@ -8,8 +8,8 @@ class TestCurrentTime(unittest.TestCase):
     def test_moscow_time_format(self):
         moscow_time = datetime.now(pytz.timezone('Europe/Moscow'))
         result = current_time()  # Simulate calling the route
-        expected = f"Current time in Moscow: \
-        {moscow_time.strftime('%Y-%m-%d %H:%M:%S')}"
+        time_str = moscow_time.strftime('%Y-%m-%d %H:%M:%S')
+        expected = f"Current time in Moscow: {time_str}"
         self.assertEqual(result, expected)
 
 
