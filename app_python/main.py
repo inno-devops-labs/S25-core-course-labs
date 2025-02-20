1a94fa8dee08849dfa07222cb4a7551aa73f0755
Kazan-Strelnikova <strelnikova.kira354@gmail.com>
2025-02-20 13:11:30 +0300

    lint fix

--- app_python/main.py
+++ app_python/main.py
@@ -9,15 +9,17 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 @app.route('/')
 def current_time():
     moscow_time = datetime.now(pytz.timezone('Europe/Moscow'))
     time_str = moscow_time.strftime('%Y-%m-%d %H:%M:%S')
-    
+
     # Log the current time request
     logger.info(f"Current time requested: {time_str}")
-    
+
     return f"Current time in Moscow: {time_str}"
+
 
 if __name__ == '__main__':
     # Log when the server starts
