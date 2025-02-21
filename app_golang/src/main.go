fc9a96930704ce329b8c0d0b2ef24fc1c52b5fca
Daniil Nikulin <d.nikulin@innopolis.university>
2025-02-22 00:51:07 +0300

    feat(lab8): bonus increment metrics in golang app

--- app_golang/src/main.go
+++ app_golang/src/main.go
@@ -6,13 +6,39 @@ import (
 	docs "github.com/danmaninc/S25-core-course-labs/app_golang/src/docs"
 	"github.com/danmaninc/S25-core-course-labs/app_golang/src/endpoints"
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+var jokePageCounter = prometheus.NewGauge(prometheus.GaugeOpts{
+	Name: "joke_page_visits",
+	Help: "Count of received jokes via webpage. Bring humour to the world!",
+})
+
+var jokeJsonCounter = prometheus.NewGauge(prometheus.GaugeOpts{
+	Name: "joke_json_visits",
+	Help: "Count of received jokes via API endpoint. Bring humour to the robots!",
+})
+
+func init() {
+	prometheus.MustRegister(jokePageCounter, jokeJsonCounter)
+}
+
+func prometheusHandler() gin.HandlerFunc {
+	h := promhttp.Handler()
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+ }
 // @BasePath /
 
 func setupRouter() *gin.Engine {
+	jokePageCounter.Set(0)
+	jokeJsonCounter.Set(0)
+
 	router := gin.Default()
 	docs.SwaggerInfo.BasePath = "/"
 
@@ -22,12 +48,14 @@ func setupRouter() *gin.Engine {
 	// Register endpoints
 	v1 := router.Group("/")
 	{
-		v1.GET("/", endpoints.JokePage)
-		v1.GET("/joke", endpoints.Joke)
+		v1.GET("/", endpoints.JokePage, func (c *gin.Context) { jokePageCounter.Inc() })
+		v1.GET("/joke", endpoints.Joke, func (c *gin.Context) { jokeJsonCounter.Inc() })
 	}
 
 	// Run router
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	router.GET("/metrics", prometheusHandler())
 
 	return router
 }
