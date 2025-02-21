44846359ad4d52c3e52e4c5c20bed772d9f17ae1
Daniil Nikulin <d.nikulin@innopolis.university>
2025-02-22 00:50:27 +0300

    feat(lab8): bonus increment metrics in python app

--- app_python/src/main.py
+++ app_python/src/main.py
@@ -10,6 +10,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from src.utils import get_time
 from src.models import Time
+from prometheus_client import Counter, CONTENT_TYPE_LATEST, REGISTRY, generate_latest
 
 
 # Create FastAPI application
@@ -18,6 +19,17 @@ app = FastAPI(description="""Web application for displaying current time in Mosc
 
 # Load templates from the corresponding directory
 templates = Jinja2Templates(directory="src/templates")
+
+# Register metrics
+time_page_visits = Counter(
+    'time_page_visits',
+    'Visits of web page containing current time in Moscow. Bring time to human!'
+)
+
+time_json_visits = Counter(
+    'time_json_visits',
+    'Visits of API endpoint containing current time in Moscow. Bring time to robots!'
+)
 
 
 @app.get(
@@ -35,6 +47,7 @@ templates = Jinja2Templates(directory="src/templates")
 )
 async def read_time(request: Request):
     """Returns web page containing current time in Moscow."""
+    time_page_visits.inc()
     current_time = get_time().strftime("%d.%m.%Y %H:%M:%S")
     return templates.TemplateResponse(
         request=request, name="time.html", context={"time": current_time}
@@ -54,5 +67,23 @@ async def read_time(request: Request):
 )
 async def read_time_json():
     """Returns current time in Moscow in JSON format."""
+    time_json_visits.inc()
     current_time = str(get_time())
     return {"time": current_time}
+
+@app.get(
+    "/metrics",
+    response_model=None,
+    status_code=status.HTTP_200_OK,
+    description="""Get latest metrics""",
+    tags=["metrics"],
+    summary="Returns latest metrics",
+    responses={
+        status.HTTP_200_OK: {
+            "description": "Latest metrics"
+        }
+    }
+)
+async def read_metrics():
+    metrics = generate_latest(registry=REGISTRY)
+    return HTMLResponse(metrics, media_type=CONTENT_TYPE_LATEST)
