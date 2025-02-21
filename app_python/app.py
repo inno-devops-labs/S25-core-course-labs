db72b2d7151b873fb3f1d377ed6cb91b56f8ed61
l.smirnova <l.smirnova@innopolis.university>
2025-02-22 01:55:13 +0300

    intermediate version

--- app_python/app.py
+++ app_python/app.py
@@ -1,20 +1,30 @@
 from flask import Flask, render_template
 from datetime import datetime
+from prometheus_client import start_http_server, Counter, generate_latest, CONTENT_TYPE_LATEST
+import threading
 import pytz
 
-app = Flask(__name__)  # initialize the Flask application
+app = Flask(__name__)
 
+request_count = Counter('http_requests_total', 'Total number of HTTP requests')
 
-@app.route("/")  # define the main page route
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
+
+@app.route("/")  
 def index():
-    # Set the time zone to Moscow
+    request_count.inc()
+
     moscow_tz = pytz.timezone("Europe/Moscow")
-    # Get the current time in format HH:MM:SS
     current_time = datetime.now(moscow_tz).strftime("%H:%M:%S")
-    # Render the html page, passing there the current time
+    
     return render_template("index.html", time=current_time)
 
+def start_prometheus():
+    start_http_server(7000) 
 
 if __name__ == "__main__":
-    # Run the application
-    app.run(host="0.0.0.0", port=5000)
+    threading.Thread(target=start_prometheus, daemon=True).start()
+    
+    app.run(host="0.0.0.0", port=5001)
