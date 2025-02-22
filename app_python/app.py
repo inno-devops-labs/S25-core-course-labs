1497bd8f65ef1e1405147e640b4fbdb408b00e6c
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2025-02-23 00:53:19 +0300

    feat: solve 7 lab

--- app_python/app.py
+++ app_python/app.py
@@ -3,27 +3,28 @@ import socketserver
 from datetime import datetime, timezone, timedelta
 import json
 
-PORT = 8000
-
 # Moscow is UTC+3
 MOSCOW_TZ = timezone(timedelta(hours=3))
 
 
 class MoscowTimeAPIHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        self.log_message("Request path: %s, method: %s", self.path, self.command)
         self.send_response(200)
         self.send_header("Content-type", "application/json")
         self.end_headers()
         moscow_time = datetime.now(MOSCOW_TZ).strftime("%Y-%m-%d %H:%M:%S")
         response = {"moscow_time": moscow_time}
+        self.log_message("Response: %s", response)
         self.wfile.write(json.dumps(response).encode("utf-8"))
 
 
-def main():
-    with socketserver.TCPServer(("", PORT), MoscowTimeAPIHandler) as httpd:
-        print(f"Serving on port {PORT}")
+def main(port: int = 8000):
+    with socketserver.TCPServer(("", port), MoscowTimeAPIHandler) as httpd:
+        print(f"Serving on port {port}")
         httpd.serve_forever()
 
 
 if __name__ == "__main__":
+    print("Starting Moscow Time API server...")
     main()
