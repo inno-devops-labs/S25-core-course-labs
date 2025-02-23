8e79afe3438c51d57ee2ffb4eb2b49e1dfb1acc5
NetPo4ki <egoroneplusovich@gmail.com>
2025-02-23 23:42:57 +0300

    feat: Lab-8 with bonus task

--- app_go/main.go
+++ app_go/main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
@@ -14,5 +15,9 @@ func main() {
 		moscowTime := time.Now().UTC().Add(3 * time.Hour).Format("2006-01-02 15:04:05")
 		c.String(http.StatusOK, fmt.Sprintf("Current time in Moscow: %s", moscowTime))
 	})
-	r.Run(":8080")
+	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
+	err := r.Run(":8080")
+	if err != nil {
+		return
+	}
 }
