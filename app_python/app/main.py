b3b95defc8fa105439699a9fffbb014aad1ca033
Fridorovich <e.nischikh@innopolis.university>
2025-02-23 20:12:09 +0500

    lab8

--- app_python/app/main.py
+++ app_python/app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from datetime import datetime
+from prometheus_fastapi_instrumentator import Instrumentator
 import pytz
 
 app = FastAPI()
 
+Instrumentator().instrument(app).expose(app)
 
-@app.get("/")
+@app.get("/time")
 def get_moscow_time():
     timezone = pytz.timezone("Europe/Moscow")
     time = datetime.now(timezone).strftime("%Y-%m-%d %H:%M:%S")
