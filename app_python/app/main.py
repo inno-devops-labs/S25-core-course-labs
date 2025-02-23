143b3c214b8e30bc5974af496e6a4e91a748653b
dExNight <am.gubaidullin@innopolis.university>
2025-02-23 21:16:10 +0300

    lab8: task 1 and task 2

--- app_python/app/main.py
+++ app_python/app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from routers.time import router as TimeRouter
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 app.include_router(TimeRouter)
+
+@app.on_event("startup")
+async def startup():
+    Instrumentator().instrument(app).expose(app)
