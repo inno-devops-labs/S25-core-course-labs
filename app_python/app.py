d8290e4385df2baaddb19ecec96649711d93bcfe
Suleiman Karim Eddin <sulimankareem@gmail.com>
2025-02-23 14:03:05 +0300

    finishing lab8

--- app_python/app.py
+++ app_python/app.py
@@ -1,15 +1,29 @@
-from flask import Flask
+from flask import Flask, Response
 from datetime import datetime
 import pytz
+from prometheus_client import CONTENT_TYPE_LATEST, Counter, generate_latest
 
 app = Flask(__name__)
 
-@app.route('/')
+# Define Prometheus counter for tracking HTTP requests
+REQUEST_COUNT = Counter(
+    "http_requests_total", "Total number of HTTP requests", ["method", "endpoint"]
+)
+
+@app.route("/")
 def show_time():
-    moscow_tz = pytz.timezone('Europe/Moscow')
-    moscow_time = datetime.now(moscow_tz).strftime('%Y-%m-%d %H:%M:%S')
+    REQUEST_COUNT.labels(method="GET", endpoint="/").inc()  # Increment counter
+    
+    moscow_tz = pytz.timezone("Europe/Moscow")
+    moscow_time = datetime.now(moscow_tz).strftime("%Y-%m-%d %H:%M:%S")
+    
     return f"<h1>Current Time in Moscow: {moscow_time}</h1>"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+@app.get("/metrics")
+def metrics():
+    REQUEST_COUNT.labels(method="GET", endpoint="/metrics").inc()  # Increment counter
+    
+    return Response(generate_latest(), content_type=CONTENT_TYPE_LATEST)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ monitoring/METRICS.md
@@ -0,0 +1,31 @@
+# Metrics
+
+
+![1740308172819](images/METRICS/1740308172819.png)
+
+
+Loki:
+
+![1740308244356](images/METRICS/1740308244356.png)
+
+
+Prometheus:
+
+![1740308375300](images/METRICS/1740308375300.png)
+
+
+
+Python app metrics:
+
+
+![1740308444866](images/METRICS/1740308444866.png)
+
+
+## Health checks
+
+
+docker-compose.yml has health checks for all services:
+
+* Interval: 30 seconds
+* Timeout: 10 seconds
+* Retries: 5
