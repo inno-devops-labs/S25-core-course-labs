9676c1664b0fdfb3e4dbdb3ac805248d3afa29d2
Niyaz <71025204+Vitalo-bit@users.noreply.github.com>
2025-02-26 12:23:23 +0300

    Lab8 app updated for monitoring

--- app_python/app.py
+++ app_python/app.py
@@ -1,15 +1,26 @@
 from flask import Flask
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST, Counter, Gauge
 from datetime import datetime
 import pytz
 
 app = Flask(__name__)
 
+REQUEST_COUNT = Counter('app_requests_total', 'Total number of requests')
+LAST_METRICS_ACCESS = Gauge('app_last_metrics_access_time', 'Last time the /metrics endpoint was accessed')
+
 
 @app.route("/")
 def moscow_time():
+    REQUEST_COUNT.inc()
     moscow_tz = pytz.timezone("Europe/Moscow")
     current_time = datetime.now(moscow_tz).strftime("%Y-%m-%d %H:%M:%S")
-    return f"<h1>Current Time in Moscow: {current_time}</h1>"
+    return f"<h1>Current time in Moscow is {current_time}</h1>"
+
+
+@app.route("/metrics")
+def metrics():
+    LAST_METRICS_ACCESS.set_to_current_time()
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
 
 
 if __name__ == "__main__":
