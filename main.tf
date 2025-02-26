b75b198b1e67816536b2c59262c3348dd7017e1a
Durmazz1 <137828308+Durmazz1@users.noreply.github.com>
2025-02-26 21:18:24 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.13"
+    }
+  }
+}
+
+provider "yandex" {
+  service_account_key_file = "key.json"
+  cloud_id                 = "ajeqihpm4itro488vb39"
+  folder_id                = "b1gg5q09ml7olese1g65"
+  zone                     = "<default_availability_zone>"
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network-1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet-1"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+  zone           = "ru-central1-d"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name        = "terraform1"
+  platform_id = "standard-v3"
+  zone        = "ru-central1-d"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd86i4aqfsl6b37ra5fs"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = file("${path.module}/meta.txt")
+  }
+}
+
+resource "yandex_compute_instance" "vm-2" {
+  name        = "terraform2"
+  platform_id = "standard-v3"
+  zone        = "ru-central1-d"
+
+  resources {
+    cores  = 4
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd86i4aqfsl6b37ra5fs"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = file("${path.module}/meta.txt")
+  }
+}
