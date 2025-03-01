395f9fccec017b6d91538beac62f402493d96829
xrvst <4182808@gmail.com>
2025-03-01 10:01:09 +0000

    lab12

--- app_python/src/routes/__init__.py
+++ app_python/src/routes/__init__.py
@@ -1,7 +1,9 @@
 from src.routes.moscow_time import router as time_router
+from src.routes.visits import router as visits_router
 
 ALL_ROUTERS = [
-    time_router
+    time_router,
+    visits_router
 ]
 
 __all__ = [
--- app_python/src/routes/moscow_time.py
+++ app_python/src/routes/moscow_time.py
@@ -2,11 +2,14 @@ from fastapi import APIRouter
 from datetime import datetime
 from pytz import timezone
 
+from src.utils.visits_manager import inc_visits
+
 
 router = APIRouter()
 
 @router.get("/")
 async def read_time():
+    inc_visits()
     moscow = timezone('Europe/Moscow')
     current_time = datetime.now(moscow).strftime("%Y-%m-%d %H:%M:%S")
     return {"Moscow Time": current_time}
