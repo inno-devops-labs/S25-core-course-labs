7e995f6fa7e90210183f7de9b269e08f6a79d826
Viktoria Patrina <v.patrina@innopolis.university>
2025-03-07 13:41:45 +0300

    fix app.py

--- app_python/app.py
+++ app_python/app.py
@@ -10,6 +10,7 @@ metrics = PrometheusMetrics(app)
 
 VISITS_FILE = "visits"
 
+
 def get_visits():
     if not os.path.exists(VISITS_FILE):
         return 0
@@ -19,36 +20,47 @@ def get_visits():
         except ValueError:
             return 0
 
+
 def save_visits(count):
     with open(VISITS_FILE, "w") as f:
         f.write(str(count))
 
+
 @app.route("/metrics")
 def metrics_endpoint():
-    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
+    return generate_latest(), 200, {"Content-Type": CONTENT_TYPE_LATEST}
+
 
 @app.route("/")
 def home():
     visits = get_visits() + 1
     save_visits(visits)
-    
-    moscow_time = datetime.now(pytz.timezone("Europe/Moscow")).strftime("%Y-%m-%d %H:%M:%S")
-    return render_template_string("""
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Moscow Time</title>
-    </head>
-    <body>
-        <h1>Current Time in Moscow</h1>
-        <p>{{ time }}</p>
-    </body>
-    </html>
-    """, time=moscow_time)
+
+    moscow_time = datetime.now(pytz.timezone("Europe/Moscow")).strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
+    return render_template_string(
+        """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Moscow Time</title>
+        </head>
+        <body>
+            <h1>Current Time in Moscow</h1>
+            <p>{{ time }}</p>
+        </body>
+        </html>
+        """,
+        time=moscow_time,
+    )
+
 
 @app.route("/visits")
 def visits():
     return f"This page has been visited {get_visits()} times."
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
