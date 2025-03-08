820fe37208a17989ad345fa6694edad36ce8f9c7
Milanaaaa <milanasirozhova@yandex.ru>
2025-03-08 13:58:34 +0300

    fixes

--- app_python/app.py
+++ app_python/app.py
@@ -36,7 +36,9 @@ def current_time():
     moscow_tz = pytz.timezone('Europe/Moscow')
     current_time = datetime.now(moscow_tz).strftime("%Y-%m-%d %H:%M:%S")
     visits = update_visits()
-    return f"<h1>Current time in Moscow: {current_time}</h1><p>Visits: {visits}</p>"
+    return "<h1>Current time in Moscow: {}</h1><p>Visits: {}</p>".format(
+        current_time, visits
+    )
 
 
 @app.route("/metrics")
