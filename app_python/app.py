c738e4a9dc6a4f8879854b94ffa173ec4ff731c5
bakinsa <bakinasa@gmail.com>
2025-03-09 23:58:00 +0300

    feat(lab12): fix app

--- app_python/app.py
+++ app_python/app.py
@@ -1,11 +1,8 @@
-"""
-Python web application that displays the current time in Moscow
-"""
-
 import logging
 from datetime import datetime
 from flask import Flask, render_template
 import pytz
+import os
 
 app = Flask(__name__)
 
@@ -13,6 +10,20 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
+
+VISITS_FILE = 'visits.txt'
+
+def read_visits():
+    if os.path.exists(VISITS_FILE):
+        with open(VISITS_FILE, 'r') as file:
+            count = file.read()
+            return int(count)
+    else:
+        return 0
+
+def write_visits(count):
+    with open(VISITS_FILE, 'w') as file:
+        file.write(str(count))
 
 @app.route("/")
 def current_time():
@@ -24,6 +35,16 @@ def current_time():
     logging.info(f"Served current time: {moscow_time}")
     return render_template("index.html", time=moscow_time)
 
+@app.route("/visits")
+def visits():
+    """
+    Function to display the number of visits
+    """
+    count = read_visits()
+    count += 1
+    write_visits(count)
+    logging.info(f"Visit count updated: {count}")
+    return render_template("visits.html", count=count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
