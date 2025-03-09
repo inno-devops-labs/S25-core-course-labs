b23b9ca975c63206f42085fe16770297ff8c467a
root <root@codenv.ru-central1.internal>
2025-03-09 13:57:07 +0000

    Lab-07

--- app_python/app/routes.py
+++ app_python/app/routes.py
@@ -1,10 +1,18 @@
 import datetime
-from flask import render_template
+from flask import render_template, request
 from app import app
+from app.logging_config import setup_logging
 
+logger = setup_logging()
 
 @app.route('/')
 def current_time():
+    log_request()
+
     moscow_timezone = datetime.timezone(datetime.timedelta(hours=3))
     moscow_time = datetime.datetime.now(moscow_timezone)
+
     return render_template('index.html', current_time=moscow_time)
+
+def log_request():
+    logger.info(f"Received request: {request.method} {request.url}")
