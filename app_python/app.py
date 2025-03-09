796fa2332f298916aed94ff4af6cf12d89e8d6ff
Tedor49 <teddy.smirnov@gmail.com>
2025-03-10 02:01:48 +0500

    lab12

--- app_python/app.py
+++ app_python/app.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone, timedelta
+from os.path import exists
 from flask import Flask, render_template
 from prometheus_flask_exporter import PrometheusMetrics
 
@@ -13,6 +14,13 @@ metrics = PrometheusMetrics(app)
 # Moscow time zone is UTC+3
 TIMEZONE = 3
 DATE_FORMAT = "%a %b %d %H:%M:%S %Y"
+
+# Log file name
+filename = "/tmp/visits"
+if not exists(filename):
+    with open(filename, "w") as f:
+        f.write("0")
+
 
 # Serve status to Prometheus
 @app.route('/status/<int:status>')
@@ -30,6 +38,19 @@ def serve_time():
     # Get time in the required timezone and format it
     time_moscow = datetime.now(timezone(timedelta(hours=TIMEZONE)))
     time_moscow = time_moscow.strftime(DATE_FORMAT)
+    
+    # Increment visit counter
+    f = open(filename, "r+")
+    count = int(f.read())
+    f.seek(0)
+    f.write(f"{count+1}")
+    f.truncate()
+    f.close()
 
     # Render the template and send it to user
     return render_template("template.html", time_msk=time_moscow)
+    
+# Display visits to main page endpoint
+@app.route("/visits")
+def visit_count():
+    return open(filename, "r").read()
