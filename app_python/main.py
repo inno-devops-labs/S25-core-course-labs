10232b737a234324ccefae4699a49f6d3f130a22
DeHopen <a2302004b23@yandex.ru>
2025-03-09 19:55:57 +0300

    lab12 solution with bonus task

--- app_python/main.py
+++ app_python/main.py
@@ -5,6 +5,22 @@ import pytz
 app = Flask(__name__)
 
 
+def get_visits():
+    try:
+        with open('visits.txt', 'r') as file:
+            visits = int(file.read())
+        return visits
+    except FileNotFoundError:
+        with open('visits.txt', 'w') as file:
+            file.write('0')
+        return 0
+
+
+def update_visits(visits):
+    with open('visits.txt', 'w') as file:
+        file.write(str(visits))
+
+
 @app.route('/')
 def index():
     tz_msk = pytz.timezone('Europe/Moscow')
@@ -13,6 +29,14 @@ def index():
     formatted_time = current_time_msk.strftime('%Y-%m-%d %H:%M:%S')
 
     return f'<h1>Current time in Moscow (MSK): {formatted_time}</h1>'
+
+@app.route('/visits')
+def get_visits_route():
+    try:
+        visits = get_visits()
+        return f'The number of visits is: {visits}'
+    except Exception as e:
+        return f'An error occurred: {str(e)}'
 
 
 if __name__ == '__main__':
