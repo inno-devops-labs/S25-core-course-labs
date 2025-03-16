e639671a3a3260277665b83da0c165753c123c85
Ismagil <absoriann@gmail.com>
2025-03-16 14:35:51 +0300

    [lab14] Implement visits count as a persistent data example

--- app_python/src/main.py
+++ app_python/src/main.py
@@ -7,7 +7,28 @@ from fastapi.templating import Jinja2Templates
 
 import datetime
 import pytz
+import os
 
+
+class Visits:
+    VISITS_PATH = "./data/visits"
+
+    def increment(self):
+        self.visits += 1
+        with open(Visits.VISITS_PATH, "w") as f:
+            f.write(str(self.visits))
+
+    def __init__(self):
+        try:
+            with open(Visits.VISITS_PATH, "r") as f:
+                self.visits = int(f.read())
+        except:
+            self.visits = 0
+            with open(Visits.VISITS_PATH, "w") as f:
+                f.write(str(self.visits))
+
+
+vis = Visits()
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
@@ -15,6 +36,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def homepage(request: Request):
+    vis.increment()
     current_date = datetime.datetime.now(pytz.timezone("Europe/Moscow"))
     return templates.TemplateResponse(
         request, "index.html", {"time": current_date.strftime("%H:%M")}
