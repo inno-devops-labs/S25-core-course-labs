9e10d1757ad312355768d0a9a4a6e59aa06c0033
Friedox <dvzvidrin48@gmail.com>
2025-03-23 22:39:17 +0300

    Update app.py

--- app_python/app.py
+++ app_python/app.py
@@ -5,11 +5,15 @@ import pytz
 
 app = Flask(__name__)
 
-counter_file = os.getenv("COUNTER_FILE", "/app/data/visits.txt")
+
+def get_counter_file():
+    return os.getenv("COUNTER_FILE", "/app/data/visits.txt")
 
 
 @app.route('/')
 def time():
+    counter_file = get_counter_file()
+
     os.makedirs(os.path.dirname(counter_file), exist_ok=True)
 
     count = 0
@@ -21,13 +25,14 @@ def time():
     with open(counter_file, "w") as f:
         f.write(str(count))
 
-    moscow_tz = pytz.timezone('Europe/Moscow')
-    moscow_time = datetime.now(moscow_tz).strftime('%Y-%m-%dT%H:%M:%S')
+    moscow_time = datetime.now(pytz.timezone('Europe/Moscow')).strftime('%Y-%m-%dT%H:%M:%S')
     return render_template('index.html', moscow_time=moscow_time)
 
 
 @app.route('/visits')
 def visits():
+    counter_file = get_counter_file()
+
     count = 0
     if os.path.exists(counter_file):
         with open(counter_file, "r") as f:
