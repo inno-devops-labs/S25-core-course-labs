52007c210121d110b22d091e2732062af063d8b4
darya <d.koncheva@innopolis.university>
2025-04-29 01:42:42 +0300

    lab 8

--- app_python/main.py
+++ app_python/main.py
@@ -1,6 +1,9 @@
 from datetime import datetime
+from time import monotonic
+
 import pytz
-from flask import Flask, render_template
+from flask import Flask, render_template, g, request
+from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
 
 TIMEZONE = "Europe/Moscow"
 TIME_FORMAT = "%Y.%m.%d %H:%M:%S"
@@ -8,13 +11,62 @@ PAGE = "index.html"
 
 app = Flask(__name__)
 
+REQUESTS_COUNT = Counter(
+    "http_requests_total", "Total count of HTTP requests", ["method", "path", "http_code"]
+)
+REQUESTS_LATENCY = Histogram(
+    "http_request_duration_seconds", "Duration in seconds of HTTP requests", ["path"]
+)
+
+
+@app.before_request
+def record_start_time():
+    """Store start time in Flask's global context"""
+    g.start_time = monotonic()
+
+
+@app.after_request
+def track_metrics(response):
+    """Update Prometheus metrics after each request"""
+    duration = monotonic() - g.start_time
+    REQUESTS_LATENCY.labels(
+        path=request.path
+    ).observe(duration)
+
+    REQUESTS_COUNT.labels(
+        method=request.method,
+        path=request.path,
+        http_code=response.status_code
+    ).inc()
+
+    return response
+
+
 @app.route("/", methods=["GET"])
 def index():
     """
     Returns html page that displays time in Moscow
     """
+    start = monotonic()
     time = datetime.now(pytz.timezone(TIMEZONE)).strftime(TIME_FORMAT)
-    return render_template(PAGE, time=time)
+    page = render_template(PAGE, time=time)
+    finish = monotonic()
+
+    REQUESTS_LATENCY.labels(path="/").observe(finish - start)
+
+    REQUESTS_COUNT.labels(
+        method="GET",
+        path="/",
+        http_code=200,
+    ).inc()
+
+    return page
+
+
+@app.route("/metrics", methods=["GET"])
+def metrics():
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
